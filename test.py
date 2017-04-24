4442892b814710d5db9eb62c1e536866814c88b9
mtslong <mofeng@netease.com>
2017-04-25 00:50:27 +0800

    datetime.datetime.now()

--- test.py
+++ test.py
@@ -4,5 +4,12 @@ demo for python
 
 import datetime
 
+a=datetime.MAXYEAR
+b=datetime.MINYEAR
+c=datetime.datetime.now()
+
 print "hello world"
+print "max.year\t",a 
+print "min.year\t",b 
+print "now\t\t",c 
 
